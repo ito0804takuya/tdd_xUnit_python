63c2b403613398573b6a4d3caa2b9d2e8d222244
ito0804takuya <ito0804takuya@gmail.com>
2021-09-04 14:42:45 +0900

    18章 完了

--- xunit.py
+++ xunit.py
@@ -1,7 +1,36 @@
-print("heelo")
+# coding: UTF-8
+# python2系では、上記の宣言がないとエラー発生する(このファイルには日本語が含まれるため)
+# 今回は(anyenv)localをpython3.9にしたので不要
 
+# 実行コマンド : python xunit.py
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.testMethod()
-print(test.wasRun)
+# class定義 --------------------------------
+class TestCase:
+  def __init__(self, name):
+    self.name = name
+  # テストメソッドを実行
+  def run(self):
+    method = getattr(self, self.name) # 自身のクラスのname属性を取得
+    method()
+
+class WasRun(TestCase): # TestCaseを継承
+  def __init__(self, name):
+    self.wasRun = None # None = null(Rubyのnil)
+    super().__init__(name)
+  def testMethod(self):
+    self.wasRun = 1
+
+class TestCaseTest(TestCase):
+  def testRunning(self):
+    # テストメソッドが呼ばれたらtrue, 呼ばれなければfalseを出力する
+    
+    # 呼び出し前 : falseを期待
+    test = WasRun("testMethod") # WasRunクラス, name = "testMethod"
+    assert(not test.wasRun)
+
+    # 呼び出し後 : trueを期待
+    test.run()
+    assert(test.wasRun)
+
+# main() --------------------------------
+TestCaseTest("testRunning").run()
